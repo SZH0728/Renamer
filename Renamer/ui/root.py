2374a7c4c58635339d462e46dcb8be3b1e14fe2c
Sun <102466350+szh0728@users.noreply.github.com>
2024-08-26 18:52:53 +0800

    refactor(root): 将Root类从tk.Tk继承更改为Process继承
    Root类已从直接扩展tk.Tk更改为现在扩展Process，这使得Root类能够处理消息队列中的消息。
    初始化过程中引入了Client实例作为参数，以促进消息传递。Root类现在通过其新方法创建tk.Tk实例，并委托消息处理给Process类。此外，提供了Main类来实例化Root类并启动事件循环。
    
    BREAKING CHANGE: Root类不再直接扩展tk.Tk，现在需要提供Client实例。
    在继承和实例化Root时必须相应地调整代码。

--- Renamer/ui/root.py
+++ Renamer/ui/root.py
@@ -4,22 +4,52 @@
 
 import tkinter as tk
 
-from messager import Client
+from messager import Client, Message, Server
 from ui.main.root import Frame
+from ui.base import Process
 
 
-class Root(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.geometry('600x400')
+class Root(Process):
+    def __init__(self, client: Client, *args, **kwargs):
+        super().__init__()
+        self.client = client
 
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
+        self.root = tk.Tk(*args, **kwargs)
+        self.root.geometry('600x400')
 
-        self.main = Frame(self)
+        self.root.grid_columnconfigure(0, weight=1)
+        self.root.grid_rowconfigure(0, weight=1)
+
+        self.main = Frame(self.root, self.client)
         self.main.grid(column=0, row=0, sticky='nsew')
+
+        self.root.after(100, self.process)
+
+        self.command_path = {
+            'main': self.main
+        }
+
+    def mainloop(self):
+        self.root.mainloop()
+
+    def process(self):
+        while not self.client.is_empty():
+            message: Message = self.client.read()
+            super().process(message)
+
+
+class Main(object):
+    def __init__(self, client: Client):
+        self.app = Root(client)
+        self.app.mainloop()
 
 
 if __name__ == '__main__':
-    app = Root()
+    from logging import basicConfig, DEBUG
+    basicConfig(level=DEBUG)
+
+    server = Server()
+    client_first, client_second = server.get_client()
+
+    app = Root(client_first)
     app.mainloop()
