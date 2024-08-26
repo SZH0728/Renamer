0a4ecce79be4a28a2daf88d8da79c15e4d707a41
Sun <102466350+szh0728@users.noreply.github.com>
2024-08-26 18:53:12 +0800

    refactor(ui): 将Client实例集成到UI组件初始化中
    重构Confirm和Form类，传递Client实例以促进UI组件与消息传递系统的集成。修改构造函数签名，以支持传递Client对象。
    
    BREAKING CHANGE: Confirm和Form类的构造函数参数已更改，现在需要一个Client实例。这可能会影响直接使用这些类的代码，而没有提供Client实例作为参数。

--- Renamer/ui/main/confirm.py
+++ Renamer/ui/main/confirm.py
@@ -5,11 +5,12 @@ import tkinter as tk
 from tkinter import ttk
 
 from ui.base import Base
+from messager import Client
 
 
 class Confirm(Base):
-    def __init__(self, root: tk.Frame | tk.Tk):
-        super().__init__(root, 'confirm')
+    def __init__(self, root: tk.Frame | tk.Tk, client: Client):
+        super().__init__(root, 'confirm', client)
 
         self.revocation_button = self.create_button(0, 0, '撤销')
         self.confirm_button = self.create_button(0, 1, '确认')
@@ -20,7 +21,4 @@ class Confirm(Base):
 
 
 if __name__ == '__main__':
-    window = tk.Tk()
-    window.geometry('600x400')
-    confirm = Confirm(window)
-    window.mainloop()
+    pass
--- Renamer/ui/main/form.py
+++ Renamer/ui/main/form.py
@@ -5,11 +5,12 @@ import tkinter as tk
 from tkinter import ttk
 
 from ui.base import Base
+from messager import Client
 
 
 class Form(Base):
-    def __init__(self, root: tk.Frame | tk.Tk):
-        super().__init__(root, 'form')
+    def __init__(self, root: tk.Frame | tk.Tk, client: Client):
+        super().__init__(root, 'form', client)
 
         self.list_box = self.create_list_box(0, 0)
         self.line_edit = self.create_line_edit(1, 0)
@@ -21,7 +22,4 @@ class Form(Base):
 
 
 if __name__ == '__main__':
-    window = tk.Tk()
-    window.geometry('600x400')
-    form = Form(window)
-    window.mainloop()
+    pass
