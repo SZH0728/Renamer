cec434b6e667aee5de41c5f03764e5d6cdc828ab
Sun <102466350+szh0728@users.noreply.github.com>
2024-08-26 13:15:14 +0800

    feat(Base): 添加处理消息路径逻辑
    实现了Base类中的process方法，该方法能够处理消息路径。路径可以指向不同的命令或子命令。如果路径包含一个点(.)，它会被拆分，允许递归调用适当的子命令。此外，确保了只有当路径存在且对应的方法或Base实例是可调用的或继承自Base时，才执行相应的命令。

--- Renamer/ui/base.py
+++ Renamer/ui/base.py
@@ -2,13 +2,17 @@
 # AUTHOR: Sun
 
 from abc import ABC, abstractmethod
+from typing import Callable
 import tkinter as tk
 from tkinter import ttk
+
+from messager import Message
 
 
 class Base(ABC):
     def __init__(self, root: tk.Frame | tk.Tk, name: str):
         self.root = tk.Frame(root, name=name, highlightbackground="black", highlightcolor="red", highlightthickness=2)
+        self.command_path: dict[str, Callable | Base] = {}
 
     def create_button(self, row: int, column: int, text: str) -> tk.Button:
         button = tk.Button(self.root, text=text, width=10)
@@ -37,6 +41,28 @@ class Base(ABC):
 
     def grid(self, *args, **kwargs):
         self.root.grid(*args, **kwargs)
+
+    def process(self, message: Message):
+        if '.' in message.path:
+            path, follow = message.path.split('.', 1)
+
+            if path not in self.command_path:
+                raise KeyError(f'{path} not in command path')
+
+            if not isinstance(self.command_path[path], Base):
+                raise TypeError(f'{self.command_path[path]} is not a Base')
+
+            message.path = follow
+            self.command_path[path].process(message)
+
+        else:
+            if message.path not in self.command_path:
+                raise KeyError(f'{message.path} not in command path')
+
+            if not callable(self.command_path[message.path]):
+                raise TypeError(f'{self.command_path[message.path]} is not a callable')
+
+            self.command_path[message.path](message)
 
 
 if __name__ == '__main__':
